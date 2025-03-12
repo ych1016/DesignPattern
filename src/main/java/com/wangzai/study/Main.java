4b09f4cc5c37b7544ae2576aabeccbf832fb0eaa
ych1016 <2877816340@qq.com>
2025-03-12 13:28:33 +0800

    feature[初始化项目]: 第一次提交

---
+++ src/main/java/com/wangzai/study/Main.java
@@ -0,0 +1,12 @@
+package com.wangzai.study;
+
+/**
+ * @author 杨灿杭
+ * @Description
+ * @create 2025-03-12 13:05
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
